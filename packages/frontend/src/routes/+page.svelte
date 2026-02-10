3547d9d69e4947adb5b93a81dfcaded62058b939
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-02-10 11:10:07 +0100

    feat(frontend): handle fetch error
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-import { NavPage } from '@podman-desktop/ui-svelte';
+import { NavPage, EmptyScreen, Button } from '@podman-desktop/ui-svelte';
 import type { PageProps } from './$types';
 import RepositoryCard from '$lib/cards/RepositoryCard.svelte';
-import type { Repository } from '@podman-desktop/extension-hummingbird-core-api';
 import HummingbirdBanner from '$lib/banners/HummingbirdBanner.svelte';
+import RepositoryCardSkeleton from '$lib/skeleton/RepositoryCardSkeleton.svelte';
+import { faWarning } from '@fortawesome/free-solid-svg-icons/faWarning';
+import { invalidateAll } from '$app/navigation';
 
 let { data }: PageProps = $props();
 
 let searchTerm: string = $state('');
-let filtered: Array<Repository> = $derived(
-  searchTerm.length > 0
-    ? data.repositories.filter(({ name }) => name.toLowerCase().includes(searchTerm.toLowerCase()))
-    : data.repositories,
-);
+
+function refresh(): Promise<void> {
+  return invalidateAll();
+}
 </script>
 
 <NavPage title="Hummingbird Catalog" searchEnabled={true} bind:searchTerm={searchTerm}>
@@ -20,15 +21,35 @@ let filtered: Array<Repository> = $derived(
     <div class="flex flex-col grow px-5 py-3">
       <HummingbirdBanner />
 
-      <div class="flex flex-col w-full">
-        <div
-          class="grid min-[920px]:grid-cols-2 min-[1180px]:grid-cols-3 gap-3"
-          role="region"
-          aria-label="Hummingbird extensions">
-          {#each filtered as repository (repository.name)}
-            <RepositoryCard object={repository} />
-          {/each}
-        </div>
+      <div class="flex flex-col w-full h-full">
+        <!-- while we fetch the repositories we display skeleton cards -->
+        {#await data.repositories}
+          <div
+            class="grid min-[920px]:grid-cols-2 min-[1180px]:grid-cols-3 gap-3">
+            {#each Array.from({ length: 10 }) as _, index (index)}
+              <RepositoryCardSkeleton/>
+            {/each}
+          </div>
+        {:then repositories}
+          {@const filtered = searchTerm.length > 0 ? repositories.filter(({ name }) => name.toLowerCase().includes(searchTerm.toLowerCase())) : repositories}
+          <div
+            class="grid min-[920px]:grid-cols-2 min-[1180px]:grid-cols-3 gap-3">
+            {#each filtered as repository (repository.name)}
+              <RepositoryCard object={repository} />
+            {/each}
+          </div>
+        {:catch err}
+          <EmptyScreen
+              icon={faWarning}
+              title="Something went wrong while fetching the Hummingbird catalog"
+              aria-label="Error while fetching the Hummingbird catalog"
+              message={err?.message ?? String(err)}
+          >
+            <div class="flex gap-2 justify-center">
+              <Button title="Retry" type="link" onclick={refresh}>Retry</Button>
+            </div>
+          </EmptyScreen>
+        {/await}
       </div>
     </div>
   {/snippet}
